93341d1ff6bc439195a91048c5f82dace0303410
SangNyungLee <lsy6747@gmail.com>
2025-07-31 18:02:26 +0900

    javaScript -> vue 변경

--- v01/app.js
+++ v01/app.js
@@ -1,11 +1,25 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+    data(){
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal(){
+            this.goals.push(this.enteredValue);
+        }
+    },
+}).mount('#app');
 
-function addGoal() {
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = enteredValue;
-    listEl.appendChild(listItemEl);
-}
-buttonEl.addEventListener('click', addGoal);
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li');
+//     listItemEl.textContent = enteredValue;
+//     listEl.appendChild(listItemEl);
+// }
+// buttonEl.addEventListener('click', addGoal);

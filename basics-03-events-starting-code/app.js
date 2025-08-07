3bfe0489c45c3c2c9163ccfecd2424a5868998d6
SangNyungLee <lsy6747@gmail.com>
2025-08-07 12:44:29 +0900

    이벤트 바인딩

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 10,
+      name: "",
+      confirmedName: "",
+    };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert("submitted!");
+    },
+    setName(e, lastName) {
+      this.name = e.target.value + " " + lastName;
+    },
+    add(num) {
+      this.counter += num;
+    },
+    reduce(num) {
+      this.counter -= num;
+    },
+  },
+});
+
+app.mount("#events");
